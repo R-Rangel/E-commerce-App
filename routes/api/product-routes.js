b3ef248810b96d2282ee99f0a72d2df303837301
R-Rangel <r.rangel.0222@gmail.com>
2023-04-06 02:40:47 -0500

    added code to server.js to sync the sequelize models to mysql db

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -131,7 +131,21 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((deletedProduct) => {
+    if (deletedProduct === 0) {
+      return res.status(404).json({ message: 'No product found with this id' });
+    }
+    res.json({ message: 'Product deleted successfully' });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
